6fc0dfb299ed81d0a60fc1743a91d70cc6950f63
Storm-cloak <raghamoghlanov@gmail.com>
2021-02-04 23:05:42 +0400

    Singly Linked List

---
+++ Linked.cpp
@@ -0,0 +1,152 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+};
+
+class LinkedList
+{
+private:
+    Node *head, *tail;
+
+public:
+    int length = 0;
+    LinkedList()
+    {
+        head = nullptr;
+        tail = nullptr;
+    }
+
+    void append(int value)
+    {
+        Node *tmp = new Node();
+        tmp->data = value;
+        tmp->next = nullptr;
+        if (head == nullptr)
+        {
+            head = tmp;
+            tail = tmp;
+        }
+        else
+        {
+            tail->next = tmp;
+            tail = tail->next;
+        }
+        length += 1;
+    }
+
+    void prepend(int value)
+    {
+        Node *tmp = new Node();
+        tmp->data = value;
+        tmp->next = nullptr;
+        if (head == nullptr)
+        {
+            tail = tmp;
+            head = tmp;
+        }
+        else
+        {
+            tmp->next = head;
+            head = tmp;
+        }
+        length += 1;
+    }
+
+    void display()
+    {
+        if (head == nullptr)
+        {
+            std::cout << "List is empty" << '\n';
+        }
+        else
+        {
+            Node *temp = new Node();
+            temp = head;
+            while (temp != nullptr)
+            {
+                std::cout << temp->data << " ";
+                temp = temp->next;
+            }
+            std::cout << '\n';
+        }
+    }
+    void insert(int index, int value)
+    {
+        Node *tmp = new Node();
+        Node *current = new Node();
+        Node *prev = new Node();
+        current = head;
+        for (int i = 0; i < index; ++i) //0 1
+        {
+            prev = current;          //1stIt    prev = head = 4, 2ndIt   prev = 2
+            current = current->next; //1stIT current = 2 , 2ndIt current = 1;
+        }
+        tmp->data = value;
+        prev->next = tmp;
+        tmp->next = current;
+        length += 1;
+        // 4 -> 2 -> 1 ->5    4 -> 2 -> 6 -> 1  -> 5
+        // 0    1    2   3
+    }
+    void erase(int index)
+    {
+        Node *current = new Node();
+        Node *prev = new Node();
+        current = head;
+        if (index == 0)
+        {
+            head = current->next;
+            delete current;
+            return;
+        }
+        else if (index < 0)
+        {
+            std::cout << "not valid index, maybe you use negative value or value greater than the length of the Linked list" << '\n';
+            return;
+        }
+        for (int i = 0; i < index; ++i)
+        {
+            prev = current;
+            current = current->next;
+        }
+        prev->next = current->next;
+        delete current;
+        length -= 1;
+    }
+    void reverse()
+    {
+        Node *current = head;
+        Node *nextnode = NULL;
+        Node *prevnode = NULL;
+        while (current != NULL)
+        {
+            // Store next
+            nextnode = current->next;
+            // Reverse current node's pointer
+            current->next = prevnode;
+            // Move pointers one position ahead.
+            prevnode = current;
+            current = nextnode;
+        }
+        head = prevnode;
+    }
+};
+
+int main()
+{
+    LinkedList L;
+    L.prepend(2);
+    L.append(1);
+    L.append(5);
+    L.prepend(4);
+    L.insert(2, 6);
+    // L.erase(1);
+    L.reverse();
+    L.display();
+    std::cout << L.length;
+    return 0;
+}
